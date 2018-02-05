a74bad88f725b8c12044aff41b64a75a4ff6aadb
Andrei Frolov <frolov@sfu.ca>
2018-02-05 15:47:04 -0800

    Slider wired to text label.

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad(); valueChanged(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func valueChanged(_ sender: Any) {
+        guard let slider = slider else { return }
+        label?.text = String(format: "%0.2f", slider.value)
+    }
 }
-
