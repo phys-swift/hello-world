bd1a51cd6938964e4a2d8a5564e5447f616247ca
Andrei Frolov <frolov@sfu.ca>
2018-02-06 14:13:12 -0800

    Added delay to simulate effects of a complex startup sequence.

--- Hello World/Hello World/AppDelegate.swift
+++ Hello World/Hello World/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        // sleep for half a second to simulate complex startup sequence
+        Thread.sleep(forTimeInterval: 0.5)
+        
         // Override point for customization after application launch.
         return true
     }
